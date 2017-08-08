5b8760eeea47e093d521297a8f34b644cc08cac6
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-08-08 13:29:30 -0700

    finished user model

--- model/user.js
+++ model/user.js
@@ -43,4 +43,33 @@ userSchema.methods.comparePasswordHash = function(password) {
 
 userSchema.methods.generateFindHash = function() {
   debug('generateFindHash');
-}
+
+  return new Promise((resolve, reject) => {
+    let tries = 0;
+
+    _generateFindHash.call(this);
+
+    function _generateFindHash(){
+      this.findHash = crypto.randomBytes(32).toString('hex');
+      this.save()
+      .then( () => resolve(this.findHash))
+      .catch( err => {
+        if(tries > 3) return reject(err);
+        tries++;
+        _generateFindHash.call(this);
+      });
+    }
+  });
+};
+
+userSchema.methods.generateToken = function() {
+  debug('generateToken');
+
+  return new Promise((resolve, reject) => {
+    this.generateFindHash()
+    .then( findHash => resolve(jwt.sign({token: findHash}, process.env.APP_SECRET)))
+    .catch(err =>reject(err));
+  });
+};
+
+module.exports = mongoose.model('user', userSchema);
